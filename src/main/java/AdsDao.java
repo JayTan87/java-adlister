7662b34d40cd89300c61f68793c24b4537b09700
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-09-05 10:08:35 -0500

    committing updates

---
+++ src/main/java/AdsDao.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdsDao implements Ads {
+
+    @Override
+    public List<Ad> all() {
+        return null;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        return null;
+    }
+}
